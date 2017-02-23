a75aa103ae036a07d958482168b8bd9dba7db177
aksbuzz <akshayjr69@gmail.com>
2017-02-23 20:21:46 +0530

    added date validation

--- pyerail/helpers.py
+++ pyerail/helpers.py
@@ -1,8 +1,13 @@
 import requests
 import sys
+from datetime import datetime
 
 
-def get_json(self, end_point, params):
+DATES = {'JAN': 'january', 'FEB': 'february', 'MAR': 'march', 'APR': 'april', 'MAY': 'may', 'JUN': 'june',
+         'JUL': 'july', 'AUG': 'august', 'SEP': 'september', 'OCT': 'october', 'NOV': 'november', 'DEC': 'dcember'}
+
+
+def get_json(end_point, params):
     '''
     Make request to http://api.erail.in/
     '''
@@ -18,14 +23,33 @@ def get_json(self, end_point, params):
     return r.json()
 
 
-def vali_date(self, date):
+def vali_date(date):
     '''
-    Validate the date in the format dd-mm-yyyy
+    Validate the date in the format DD-MMM-YYYY
 
-    Eg "5-sep-2016"
+    Eg "5-SEP-2016"
 
     Check request ? & where it comes.
 
     Check if None is valid in empty com=ndition
     '''
 
+    date_, month, year = list(date.split('-'))
+
+    month = DATES[month]
+
+    date = date_ + "-" + month + "-" + year
+
+    try:
+        
+        datetime.strptime(date,'%d-%B-%Y')
+
+        return True
+
+    except ValueError:
+        
+        return "invalid date format >> must be in 'DD-MMM-YYYY'"
+
+if __name__ == '__main__':
+    
+    assert vali_date('5-SEP-2016')
